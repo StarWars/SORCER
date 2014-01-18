bedae817acc5c368052cedc56045661a03c8d3b1
Michal S. <proprogramista@gmail.com>
2014-01-18 06:05:35 +0100

    Rozwiniecie testow

--- iGrid/modules/examples/stringer/sorcer/src/sorcer/stringer/junit/StringerTestPro.java
+++ iGrid/modules/examples/stringer/sorcer/src/sorcer/stringer/junit/StringerTestPro.java
@@ -1,25 +1,23 @@
 package sorcer.stringer.junit;
 
 import static sorcer.eo.operator.context;
-import static sorcer.eo.operator.exert;
-import static sorcer.eo.operator.get;
 import static sorcer.eo.operator.in;
-
 import static sorcer.eo.operator.sig;
 import static sorcer.eo.operator.task;
-import static sorcer.eo.operator.srv;
-import static sorcer.eo.operator.value;
 
 import java.rmi.RMISecurityManager;
 import java.util.logging.Logger;
 
 import org.junit.Test;
 
-import sorcer.stringer.provider.StringerSetOne;
-import sorcer.stringer.provider.StringerSetOneProvider;
 import sorcer.core.SorcerConstants;
+import sorcer.core.exertion.ObjectJob;
+import sorcer.service.Exertion;
+import sorcer.service.Job;
 import sorcer.service.ServiceExertion;
 import sorcer.service.Task;
+import sorcer.stringer.provider.StringerSetOne;
+import sorcer.stringer.provider.StringerSetTwo;
 import sorcer.util.Sorcer;
 
 
@@ -42,40 +40,45 @@ public class StringerTestPro implements SorcerConstants {
 	
 	
 	@Test
-	public void addTest() throws Exception {
+	public void Test() throws Exception {
 		
 		logger.info("Test stringer set one'a");
 		
-		Task t1 = task(
-				"t1",
-				sig("toUpperCase", StringerSetOne.class, "StringerSetOneProvider"),
-				context("toupper", in("toupper/string1", "kuku")));
+		Task t1 = task("to upper", 
+				sig("toUpperCase", StringerSetOne.class, "StringerSetOneProvider"), 
+				context("toupper", 
+						in("toupper/string1", "kuku2")));
 		logger.info("Task1: "+t1.getContext());
-		t1 = exert(t1);
-		logger.info("t1 context: "+context(t1));
-		logger.info("t1 value: "+get(t1, "toupper/string1/result"));
-		/*
+//		t1 = exert(t1);
+//		logger.info("t1 context: "+context(t1));
+//		logger.info("t1 value: "+get(t1, "toupper/string1/result"));
+		
 		Task t2 = task(
-				"ToLower",
-				sig("toLowerCase", StringerSetOneProvider.class, "StringerSetOneProvider"),
-				context("toLowerCase"));
+				"space split",
+				sig("getSpaceSplit", StringerSetTwo.class, "StringerSetTwoProvider"),
+				context("spacesplit", in("spacesplit/string2", "kuku2")));
 		logger.info("Task2: "+t2.getContext());
+		
+		
 		
 		Exertion job = new ObjectJob("Jobs");
 		job.addExertion(t1);
 		job.addExertion(t2);
 		
-		t1.getContext().connect("val1", "a", t2.getContext());
-		t1.getContext().connect("val2", "b", t2.getContext());
+		t2.getContext().connect("val1", "toupper/string1", t1.getContext());
+		t1.getContext().connect("val2", "spacesplit/string2", t2.getContext());
+		
+		
+		/*
 		t1.getContext().connect("val3", "c", t2.getContext());
 		t1.getContext().connect("val4", "d", t2.getContext());
 		t1.getContext().connect("val5", "e", t2.getContext());
 
 		t2.getContext().connect("avg", "a", t3.getContext());
-		
+		*/
 		job = job.exert();
 		logger.info("JOB :"+((Job)job).getJobContext());
-		*/
+		
 	
 		
 	}

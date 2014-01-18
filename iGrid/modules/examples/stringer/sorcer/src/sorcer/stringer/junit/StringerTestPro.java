843eb16155df6ce89b9ab4fe4f28eb883f2a47d4
Michal S. <proprogramista@gmail.com>
2014-01-18 16:02:38 +0100

    poprawny test

--- iGrid/modules/examples/stringer/sorcer/src/sorcer/stringer/junit/StringerTestPro.java
+++ iGrid/modules/examples/stringer/sorcer/src/sorcer/stringer/junit/StringerTestPro.java
@@ -4,6 +4,10 @@ import static sorcer.eo.operator.context;
 import static sorcer.eo.operator.in;
 import static sorcer.eo.operator.sig;
 import static sorcer.eo.operator.task;
+import static sorcer.eo.operator.exert;
+import static sorcer.eo.operator.job;
+import static sorcer.eo.operator.get;
+import static sorcer.eo.operator.value;
 
 import java.rmi.RMISecurityManager;
 import java.util.logging.Logger;
@@ -17,6 +21,7 @@ import sorcer.service.Job;
 import sorcer.service.ServiceExertion;
 import sorcer.service.Task;
 import sorcer.stringer.provider.StringerSetOne;
+import sorcer.stringer.provider.StringerSetThree;
 import sorcer.stringer.provider.StringerSetTwo;
 import sorcer.util.Sorcer;
 
@@ -32,12 +37,36 @@ public class StringerTestPro implements SorcerConstants {
 		System.setProperty("java.security.policy", Sorcer.getHome()
 				+ "/configs/policy.all");
 		System.setSecurityManager(new RMISecurityManager());
-		Sorcer.setCodeBase(new String[] { "StringerSetOneProvider.jar"});
+		Sorcer.setCodeBase(new String[] { "StringerSetOneProvider.jar", "StringerSetTwoProvider.jar", "StringerSetThreeProvider.jar"});
 		System.out.println("CLASSPATH :" + System.getProperty("java.class.path"));
 		System.setProperty("java.protocol.handler.pkgs", "sorcer.util.url|org.rioproject.url");
 	}
 	
+	@Test
+	public void toLowerTest() throws Exception{
+		Task t1 = task("to lower", 
+				sig("toLowerCase", StringerSetOne.class, "StringerSetOneProvider"), 
+				context("tolower", 
+						in("tolower/string1", "QwEsDaQ")));
+		
+		logger.info("to lower "+t1.getContext());
+		t1 = exert(t1);
+		logger.info("to lower context: "+context(t1));
+		logger.info("to lower value: "+get(t1, "tolower/string1/result"));
+	}
 	
+	@Test
+	public void firstToUpperTest() throws Exception{
+		Task t1 = task("first to upper", 
+				sig("firstToUpper", StringerSetOne.class, "StringerSetOneProvider"), 
+				context("firsttoupper", 
+						in("firsttoupper/string1", "qWERTS")));
+		
+		logger.info("first to upper "+t1.getContext());
+		t1 = exert(t1);
+		logger.info("first to upper context: "+context(t1));
+		logger.info("first to upper value: "+get(t1, "firsttoupper/string1/result"));
+	}
 	
 	@Test
 	public void Test() throws Exception {
@@ -48,34 +77,43 @@ public class StringerTestPro implements SorcerConstants {
 				sig("toUpperCase", StringerSetOne.class, "StringerSetOneProvider"), 
 				context("toupper", 
 						in("toupper/string1", "kuku2")));
-		logger.info("Task1: "+t1.getContext());
-//		t1 = exert(t1);
-//		logger.info("t1 context: "+context(t1));
-//		logger.info("t1 value: "+get(t1, "toupper/string1/result"));
+		
+		//logger.info("Task1: "+t1.getContext());
 		
 		Task t2 = task(
 				"space split",
 				sig("getSpaceSplit", StringerSetTwo.class, "StringerSetTwoProvider"),
-				context("spacesplit", in("spacesplit/string2", "kuku2")));
-		logger.info("Task2: "+t2.getContext());
+				context("spacesplit", in("spacesplit/string2")));
+		//logger.info("Task2: "+t2.getContext());
 		
+		Task t3 = task("make reverse",
+				sig("makeReverse", StringerSetThree.class, "StringerSetThreeProvider"),
+				context("makereverse", in("makereverse/string3")));
+		//logger.info("Task3: "+t3.getContext());
 		
+		Task t4 = task("to lower", 
+				sig("toLowerCase", StringerSetOne.class, "StringerSetOneProvider"), 
+				context("tolower", 
+						in("tolower/string1")));
+		Task t5 = task("have a great day",
+					sig("addSmiley", StringerSetTwo.class, "StringerSetTwoProvider"),
+					context("smiley",
+							in("smiley/string2")));
+		
+		//logger.info("Task4: "+t4.getContext());
+		t1 = exert(t1);
+		t1.getContext().connect("toupper/string1/result", "spacesplit/string2", t2.getContext());
+		//t2.getContext().connect("spacesplit/string2/result", "makereverse/string3", t3.getContext());
+		t2.getContext().connect("spacesplit/string2/result", "tolower/string1", t4.getContext());
+		t4.getContext().connect("tolower/string1/result", "smiley/string2", t5.getContext());
 		
 		Exertion job = new ObjectJob("Jobs");
 		job.addExertion(t1);
 		job.addExertion(t2);
-		
-		t2.getContext().connect("val1", "toupper/string1", t1.getContext());
-		t1.getContext().connect("val2", "spacesplit/string2", t2.getContext());
-		
-		
-		/*
-		t1.getContext().connect("val3", "c", t2.getContext());
-		t1.getContext().connect("val4", "d", t2.getContext());
-		t1.getContext().connect("val5", "e", t2.getContext());
+		job.addExertion(t4);
+		job.addExertion(t5);
+		//job.addExertion(t3);
 
-		t2.getContext().connect("avg", "a", t3.getContext());
-		*/
 		job = job.exert();
 		logger.info("JOB :"+((Job)job).getJobContext());
 		
